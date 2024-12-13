6c95e8a1f698fc5667a4fe09d65576c4e13c8a05
Julien3556 <julius.clairay@gmail.com>
2024-12-13 13:08:54 +0100

    debug

--- Game.java
+++ Game.java
@@ -5,6 +5,7 @@
 */
 
 import java.util.*;
+import java.io.FileNotFoundException;
 
 public class Game {
     /* L'affichage et la lecture d'entrée avec l'interface de jeu se fera entièrement via l'attribut display de la classe Game.
